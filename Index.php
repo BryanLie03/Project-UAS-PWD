aef10d20ac554c9833daf5d84d7bf5f99e25ca11
Bryan Lie <bryanlie@itbss.ac.id>
2026-06-08 21:44:45 +0700

    Header

--- Index.php
+++ Index.php
@@ -1,30 +1,6 @@
-<!doctype html>
-<html lang="id">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Gereja Yesus Sejati Pontianak</title>
-    <link rel="stylesheet" href="CSS/style.css" /> 
-    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script> 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" /> 
-  </head>
-  
-  <body>
-    <header>
-      <nav class="navbar">
-        <a href="Gereja-Yesus-Sejati-Pontianak.html"> <div class="logo">
-            <img src="Assets/img/Logo.png" alt="Logo" class="logo-awal" />
-            <img src="Assets/img/Logo-scrolled.png" alt="Logo" class="logo-scrolled" />
-          </div>
-        </a>
-        <ul class="nav-links">
-          <li><a href="#dasar-kepercayaan">Dasar Kepercayaan</a></li>
-          <li><a href="#sejarah">Sejarah GYS Pontianak</a></li>
-          <li><a href="#kegiatan">Kegiatan</a></li>
-          <li><a href="#jadwal-ibadah">Jadwal Ibadah</a></li>
-        </ul>
-      </nav>
-    </header>
+<?php
+include "header.php";
+?>
 
     <main>
       <section class="hero">
