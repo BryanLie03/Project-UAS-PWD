179d462da6573c2526ee08396fabd0004a2b7507
clementjeremiahjonatan <clementjeremiahjonatan@itbss.ac.id>
2026-07-12 22:06:00 +0700

    nomor whatsapp

--- footer.php
+++ footer.php
@@ -16,10 +16,15 @@
             <i class="fa-solid fa-envelope"></i>
             <a href="https://mail.google.com/mail/?view=cm&fs=1&to=pontianak@gys.or.id&su=Tanya%20Produk&body=Shalom,%20saya%20ingin%20bertanya" target="_blank">Hubungi Kami</a>
           </div>
+          <div class="footer-item">
+            <i class="fa-brands fa-whatsapp"></i>
+            <a href="https://wa.me/6285386828683" target="_blank">Whatsapp</a>
+          </div>
         </div>
+      </div>
 
-        <div class="footer-right">
-          <h3>Sosial Media</h3>
+      <div class="footer-right">
+        <h3>Sosial Media</h3>
           <div class="footer-item">
             <i class="fa-brands fa-instagram"></i>
             <a href="https://www.instagram.com/gys.pontianak?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank">@gys.pontianak</a>
